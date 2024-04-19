2e9fbd5ed73bef5a4bfd9a0f81a5cf11824b1153
Alinka123 <alinka333@gmail.com>
2024-04-19 14:34:19 +0100

    gh

---
+++ js/task-6.js
@@ -0,0 +1,39 @@
+const controls = document.getElementById('controls');
+const boxesContainer = document.getElementById('boxes');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  const boxSize = 30;
+  const boxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${boxSize + i * 10}px`;
+    box.style.height = `${boxSize + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+controls.addEventListener('click', (event) => {
+  if (event.target.dataset.create) {
+    const amount = Number(controls.querySelector('input').value);
+    if (amount >= 1 && amount <= 100) {
+      createBoxes(amount);
+      controls.querySelector('input').value = '';
+    }
+  } else if (event.target.dataset.destroy) {
+    destroyBoxes();
+  }
+});
